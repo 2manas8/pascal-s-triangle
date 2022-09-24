a8a37cb01453755aa575289ec9fc28a43d334c3b
2manas8 <114216265+2manas8@users.noreply.github.com>
2022-09-24 14:07:39 +0530

    Create triangle.cpp

---
+++ triangle.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+
+	//user input
+	int num;
+	std::cin >> num;
+
+	//processing start
+	int x;
+	for (int i = 0; i < num; i++) {
+
+		//initial spaces
+		for (int spc = 1; spc < num - i; spc++) {
+			std::cout << " ";
+		}
+
+		//numbers output
+		for (int j = 0; j <= i; j++) {
+			if (j == 0 || j == i) {
+				x = 1;
+			}
+			else {
+				x *= (i - j + 1) / j;
+			}
+			std::cout << x << " ";
+		}
+		std::cout << "\n";
+	}
+	return 0;
+}
